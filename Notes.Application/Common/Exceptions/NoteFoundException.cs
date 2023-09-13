532787562a7e4b17cf3eff7ce3597a53206d7bd3
Nikita_Hnitetskyi <106909771+NikitaHnitetskyi@users.noreply.github.com>
2023-09-13 19:53:09 +0300

    add CRUD

---
+++ Notes.Application/Common/Exceptions/NoteFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Common.Exceptions
+{
+    public class NoteFoundException : Exception
+    {
+        public NoteFoundException(string name, object key)
+            : base($"Entity \"{name}\"({key} NoteFoundException found.")
+        {
+
+        }
+    }
+}
---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHendler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Application.Common.Exceptions;
+
+
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHendler : IRequestHandler<UpdateNoteCommand, Unit>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public UpdateNoteCommandHendler(INotesDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note => note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NoteFoundException(nameof(Note), request.Id);
+            }
+
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+
+            return Unit.Value;
+        }
+    }
+}
