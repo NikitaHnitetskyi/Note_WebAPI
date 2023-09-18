a098b4368edf8976e4f83e90776e80cad70b081f
Yarosikxd <yarikkstenk@gmail.com>
2023-09-18 15:25:43 +0300

    dependency inversion settings
    Setting up primary classes such as Program

--- Notes.Persistence/DependencyInjection.cs
+++ Notes.Persistence/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,17 @@ using System.Threading.Tasks;
 
 namespace Notes.Persistence
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddPersistence (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INotesDbContext>(provider => provider.GetService<NotesDbContext>());
+            return services;
+        }
     }
 }
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//using Notes.Application;
+//using Notes.Application.Common.Mappings;
+//using Notes.Application.Interfaces;
+//using Notes.Persistence;
 
-app.MapGet("/", () => "Hello World!");
+//var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+//// Add services to the container.
+////var scope = builder.Services.BuildServiceProvider().CreateScope();
+//builder.Services.AddApplication();
+//builder.Services.AddControllers();
+//builder.Services.AddPersistence(builder.Configuration);
+//builder.Services.AddAutoMapper(config =>
+//{
+//    config.AddProfile(new AssemblyMappingProfile(typeof(Program).Assembly));
+//    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+//});
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll", policy =>
+//    {
+//        policy.AllowAnyHeader();
+//        policy.AllowAnyMethod();
+//        policy.AllowAnyOrigin();
+//    });
+//});
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    try
+//    {
+//        var context = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
+//        DbInitializer.Initialize(context);
+//    }
+//    catch (Exception)
+//    {
+//    }
+//}
+
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+//app.UseCors("AllowAll");
+//app.MapControllers();
+
+//app.Run();
