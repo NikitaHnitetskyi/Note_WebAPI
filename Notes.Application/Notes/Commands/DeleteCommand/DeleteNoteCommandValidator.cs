734fd7511252d983f8dcc3b80c3d553cb5e8603c
Yarosikxd <yarikkstenk@gmail.com>
2023-09-19 15:24:50 +0300

    Add Validation

---
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿
+
+using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    public class DeleteNoteCommandValidator: AbstractValidator<DeleteNoteCommand>
+    {
+        public DeleteNoteCommandValidator() 
+        {
+            RuleFor(deleteNoteCommmand => deleteNoteCommmand.Id).NotEqual(Guid.Empty);
+            RuleFor(deleteNoteCommand => deleteNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Notes.Application.Notes.Commands.DeleteCommand;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryValidator : AbstractValidator<GetNoteListQuery>
+    {
+        public GetNoteListQueryValidator()
+        {
+            RuleFor(x => x.UserId)
+        }
+    }
+}
