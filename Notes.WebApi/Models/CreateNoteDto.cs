ac425a45dd2695563be9bffca1629abb2c5edb6b
Yarosikxd <yarikkstenk@gmail.com>
2023-09-19 14:12:10 +0300

    Add controllers
    NoteController and BaseController

---
+++ Notes.WebApi/Models/CreateNoteDto.cs
@@ -0,0 +1,20 @@
+﻿using Notes.Application.Common.Mappings;
+using AutoMapper;
+using Notes.Application.Notes.Commands.CreateNote;
+
+namespace Notes.WebApi.Models
+{
+    public class CreateNoteDto : IMapWith<CreateNodeCommands>
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNoteDto, CreateNodeCommands>()
+                .ForMember(noteCommand => noteCommand.Title,
+                opt => opt.MapFrom(noteDto => noteDto.Title))
+                .ForMember(noteCommand => noteCommand.Details,
+                opt => opt.MapFrom(noteDto => noteDto.Details));
+        }
+    }
+}
